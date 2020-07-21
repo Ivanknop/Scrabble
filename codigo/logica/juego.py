3c7d2606e745275bc40998c31787896e87e1b192
Iván Knopoff <59924602+Ivanknop@users.noreply.github.com>
2020-07-21 12:23:30 -0300

    ahora después de guardar se sigue jugando

--- codigo/logica/juego.py
+++ codigo/logica/juego.py
@@ -316,7 +316,6 @@ def lazo_principal(jugador, cargar_partida=True):
                         archivo_partida = Juego_Guardado(ruta_guardado, unTablero, jugador.getNombre(), atril_jugador, atril_pc, bolsa_fichas, jugador.getPuntaje(), puntaje_pc, interfaz.getTiempoRestante(), preferencias, cant_cambiar, jugador.getAvatar(), palabras_jugador, palabras_pc, jugador.getDificultad())
                         archivo_partida.crear_guardado()
                         partida_existente = True
-                        jugar = False
                 instante = interfaz.paralizarTimer(instante)
 
             #-----EVENTO: Información sobre la partida-----
