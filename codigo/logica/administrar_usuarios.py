4c58794e14151436fe4efdf182587b494242cbfc
Iván Knopoff <59924602+Ivanknop@users.noreply.github.com>
2020-08-05 09:48:53 -0300

    agregados comentarios

--- codigo/logica/administrar_usuarios.py
+++ codigo/logica/administrar_usuarios.py
@@ -3,6 +3,9 @@ import json
 from codigo.interfaz import configuracion_personalizada
 
 def cargar_usuarios():
+    '''
+    Retorna una lista de usuarios, con los datos de un archivo JSON o vacía.
+    '''
     try:
         directorio_usuarios = os.path.join('guardados', 'usuarios.json')
         with open(directorio_usuarios, 'r') as archivo:
@@ -12,11 +15,18 @@ def cargar_usuarios():
     return usuarios
 
 def guardar_usuarios(usuarios):
+    '''
+    Almacena en un JSON la lista de usuarios
+    '''
     directorio_usuarios = os.path.join('guardados', 'usuarios.json')
     with open(directorio_usuarios, 'w') as archivo:
         json.dump(usuarios, archivo)
 
 def eliminar_partida(partida, usuarios):
+    '''
+    Recibe como parámetro una partida seleccionada y los usuarios y elimina la partida de dicha lista.
+    Luego invoca a guardar_usuarios()
+    '''
     usuarios.remove(partida)
     guardar_usuarios(usuarios)
     filename_partida = os.path.join('guardados', f'partida_{partida}')
